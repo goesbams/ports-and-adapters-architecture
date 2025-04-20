7876e2a85d537bf4d0de23291c4c6386366eb4a4
goesbams <goesbamsday@gmail.com>
2025-04-20 19:39:36 +0700

    feat: add wallet repository

---
+++ internal/ports/wallet_repository.go
@@ -0,0 +1,24 @@
+package ports
+
+import (
+	"context"
+	"ports-and-adapters-architecture/internal/domain"
+)
+
+// WalletRepository defines the port for wallet data operations
+type WalletRepository interface {
+	// FindByID retrieves a wallet by its ID
+	FindByID(ctx context.Context, id int) (*domain.Wallet, error)
+
+	// FindByUserID retrieves all wallet for a user
+	FindByUserID(ctx context.Context, UserID int) ([]*domain.Wallet, error)
+
+	// Save creates or updates a wallet
+	Save(ctx context.Context, wallet *domain.Wallet) error
+
+	// UpdateBalance updates only the wallet balance
+	UpdateBalance(ctx context.Context, walletID int, newBalance int) error
+
+	// Delete removes a wallet
+	Delete(ctx context.Context, id int) error
+}
