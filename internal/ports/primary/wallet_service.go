b6256a560e59211bda57fc4881aa1e8125f8c51f
goesbams <goesbamsday@gmail.com>
2025-04-21 13:26:45 +0700

    feat: add wallet service interface (driving ports)

---
+++ internal/ports/primary/wallet_service.go
@@ -0,0 +1,40 @@
+package ports
+
+import (
+	"context"
+	"ports-and-adapters-architecture/internal/domain"
+)
+
+// WalletService defines the contract for wallet application service
+type WalletService interface {
+	// CreateWallet creates a new wallet for a user
+	CreateWallet(ctx context.Context, userID int, currencyCode, description string) (*domain.Wallet, error)
+
+	// GetWallet retrieves a wallet by ID
+	GetWallet(ctx context.Context, walletID int) (*domain.Wallet, error)
+
+	// GetWalletByUserID retrieves all wallets for a user
+	GetWalletsByUserId(ctx context.Context, userID int) ([]*domain.Wallet, error)
+
+	// Deposit add funds to a wallet
+	Deposit(ctx context.Context, walletID int, amount int, description string) (*domain.Transaction, error)
+
+	// Withdraw removes funds from a wallet
+	Withdraw(ctx context.Context, walletID int, amount int, description string) (*domain.Transaction, error)
+
+	// Transfer transfers fund from one wallet to another
+	Transfer(
+		ctx context.Context,
+		fromWalletID int,
+		toWalletID int,
+		amount int,
+		description string,
+	) (*domain.Transaction, error)
+
+	// GetTransactionHistory retrieves transaction history for a wallet
+	GetTransactionHistory(
+		ctx context.Context,
+		walletID int,
+		limit, offset int,
+	) ([]*domain.Transaction, int, error)
+}
