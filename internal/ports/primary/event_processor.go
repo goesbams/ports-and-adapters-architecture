12280de54443eed2aa20101bc0a50ead8e9b1358
goesbams <goesbamsday@gmail.com>
2025-04-22 20:26:48 +0700

    feat: add event processor interface - primary port

---
+++ internal/ports/primary/event_processor.go
@@ -0,0 +1,17 @@
+package ports
+
+import "context"
+
+// EventProcessor defines the contract for event processing service
+type EventProcessor interface {
+
+	// Start registers all event handlers and starts consuming events
+	Start(ctx context.Context) error
+
+	// Stop stops consuming events
+	Stop() error
+
+	// HandleEvent process a specific event
+	// This is generaaly not called directly but is useful for testing
+	// HandleEvent(ctx context.Context, topic string, event Event) error
+}
