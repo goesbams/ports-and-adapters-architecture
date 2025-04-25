cc512e5e3eebcdb5bbe6ab06ea57ee693f49a9b6
goesbams <goesbamsday@gmail.com>
2025-04-25 07:36:21 +0700

    feat: update handle event processore primary port

--- internal/ports/primary/event_processor.go
+++ internal/ports/primary/event_processor.go
@@ -1,6 +1,9 @@
 package ports
 
-import "context"
+import (
+	"context"
+	infraports "ports-and-adapters-architecture/internal/ports/secondary/infrastructure"
+)
 
 // EventProcessor defines the contract for event processing service
 type EventProcessor interface {
@@ -13,5 +16,5 @@ type EventProcessor interface {
 
 	// HandleEvent process a specific event
 	// This is generaaly not called directly but is useful for testing
-	// HandleEvent(ctx context.Context, topic string, event Event) error
+	HandleEvent(ctx context.Context, topic string, event infraports.Event) error
 }
